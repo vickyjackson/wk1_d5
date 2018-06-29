b9c40046a604a52f13cd1c6c33bb46e029a3469f
Vicky Jackson <vicky@digitalstuff.net>
2018-06-29 16:59:51 +0100

    Completed some of the tasks with tests passing

---
+++ weekend_homework/start_point/pet_shop.rb
@@ -0,0 +1,37 @@
+#
+def pet_shop_name(petshop)
+  return petshop[:name]
+end
+
+def total_cash(petshop)
+  return petshop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(petshop, cash)
+  new_total_cash = petshop[:admin][:total_cash] += cash
+  return new_total_cash
+end
+
+def pets_sold(petshop)
+  return petshop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(petshop, pets_sold)
+  new_pets_sold_total = petshop[:admin][:pets_sold] += pets_sold
+  return new_pets_sold_total
+end
+
+def stock_count(petshop)
+  stock = petshop[:pets].length
+  return stock
+end
+
+def pets_by_breed(petshop, breed)
+  pets_of_that_breed = []
+  for pet in petshop[:pets]
+    if pet[:breed] == breed
+      pets_of_that_breed << pet
+    end
+  end
+  return pets_of_that_breed
+end
